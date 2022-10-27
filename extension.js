eca326ff9eb351a47005124835de8ce5843371cb
Alexander Sosedkin <monk@unboiled.info>
2022-10-28 01:38:42 +0200

    update for Gnome 43

--- extension.js
+++ extension.js
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2021 Alexander Sosedkin <monk@unboiled.info>
+// SPDX-FileCopyrightText: 2021-2022 Alexander Sosedkin <monk@unboiled.info>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 const Main = imports.ui.main;
@@ -8,30 +8,55 @@ let h1, h2;
 function init() {}
 
 function enable() {
-	let menu = Main.panel.statusArea.aggregateMenu._volume._volumeMenu;
+	let menu;
+	if (Main.panel.statusArea.quickSettings) {
+		menu = Main.panel.statusArea.quickSettings._volume;
+		h2 = menu._input.connect('stream-updated', _autoupdate_visibility);
+	} else {
+		menu = Main.panel.statusArea.aggregateMenu._volume._volumeMenu;
+		h2 = menu.connect('input-visible-changed', _autoupdate_visibility);
+	}
 	h1 = menu._output.connect('stream-updated', _autoupdate_visibility);
-	h2 = menu.connect('input-visible-changed', _autoupdate_visibility);
 	_autoupdate_visibility();
 }
 
 function disable() {
-	let menu = Main.panel.statusArea.aggregateMenu._volume._volumeMenu;
+	let menu;
+	if (Main.panel.statusArea.quickSettings) {
+		menu = Main.panel.statusArea.quickSettings._volume;
+		menu._input.disconnect(h2);
+	} else {
+		menu = Main.panel.statusArea.aggregateMenu._volume._volumeMenu;
+		menu.disconnect(h2);
+	}
 	menu._output.disconnect(h1);
-	menu.disconnect(h2);
 	_set_visibility(true);
 }
 
 function _set_visibility(show) {
-	let volume = Main.panel.statusArea.aggregateMenu._volume;
-	if (show)
+	let volume;
+	if (Main.panel.statusArea.quickSettings) {
+		volume = Main.panel.statusArea.quickSettings._volume;
+	} else {
+		volume = Main.panel.statusArea.aggregateMenu._volume;
+	}
+	if (show) {
 		volume.show();
-	else
+	} else {
 		volume.hide();
+	}
 }
 
 function _autoupdate_visibility() {
-	let volume = Main.panel.statusArea.aggregateMenu._volume;
-	let menu = volume._volumeMenu;
+	let volume;
+	let menu;
+	if (Main.panel.statusArea.quickSettings) {
+		volume = Main.panel.statusArea.quickSettings._volume;
+		menu = volume;
+	} else {
+		volume = Main.panel.statusArea.aggregateMenu._volume;
+		menu = volume._volumeMenu;
+	}
 	let input_visible = volume._inputIndicator.visible;
 
 	let output_muted = false;
