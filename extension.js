7a8d3c3d66e20e28a8aa8d55faf6bc0fa5582405
Alexander Sosedkin <monk@unboiled.info>
2023-10-01 12:15:16 +0200

    rewrite for GNOME 45

--- extension.js
+++ extension.js
@@ -1,72 +1,105 @@
-// SPDX-FileCopyrightText: 2021-2022 Alexander Sosedkin <monk@unboiled.info>
+// SPDX-FileCopyrightText: 2021-2023 Alexander Sosedkin <monk@unboiled.info>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-const Main = imports.ui.main;
-let h1, h2;
+import GLib from 'gi://GLib';
+import {panel} from 'resource:///org/gnome/shell/ui/main.js';
 
 
-function init() {}
+export default class AutohideVolume {
+	h_io1;
+	h_ic1;
+	h_ic2;
+	h_ic3;
+	h_ic4;
+	h_oo1;
+	h_oc1;
+	h_oc2;
 
-function enable() {
-	let menu;
-	if (Main.panel.statusArea.quickSettings) {
-		menu = Main.panel.statusArea.quickSettings._volume;
-		h2 = menu._input.connect('stream-updated', _autoupdate_visibility);
-	} else {
-		menu = Main.panel.statusArea.aggregateMenu._volume._volumeMenu;
-		h2 = menu.connect('input-visible-changed', _autoupdate_visibility);
+	init() {}
+
+	enable() {
+		GLib.idle_add(GLib.PRIORITY_DEFAULT, (() => {
+		    if (!panel.statusArea.quickSettings._volumeOutput)
+			return GLib.SOURCE_CONTINUE;
+		    this.late_enable();
+		    return GLib.SOURCE_REMOVE;
+		}).bind(this));
 	}
-	h1 = menu._output.connect('stream-updated', _autoupdate_visibility);
-	_autoupdate_visibility();
-}
 
-function disable() {
-	let menu;
-	if (Main.panel.statusArea.quickSettings) {
-		menu = Main.panel.statusArea.quickSettings._volume;
-		menu._input.disconnect(h2);
-	} else {
-		menu = Main.panel.statusArea.aggregateMenu._volume._volumeMenu;
-		menu.disconnect(h2);
+	late_enable() {
+		let input = panel.statusArea.quickSettings._volumeInput;
+		let output = panel.statusArea.quickSettings._volumeOutput;
+		this.h_ii1 = input._input.connect(
+			'stream-updated', AutohideVolume._autoupdate_visibility
+		);
+		this.h_ic1 = input._control.connect(
+			'state-changed', AutohideVolume._autoupdate_visibility
+		);
+		this.h_ic2 = input._control.connect(
+			'active-input-update',
+			AutohideVolume._autoupdate_visibility
+		);
+		this.h_ic3 = input._control.connect(
+			'stream-added',
+			AutohideVolume._autoupdate_visibility
+		);
+		this.h_ic4 = input._control.connect(
+			'stream-removed',
+			AutohideVolume._autoupdate_visibility
+		);
+		this.h_oo1 = output._output.connect(
+			'stream-updated', AutohideVolume._autoupdate_visibility
+		);
+		this.h_oc1 = output._control.connect(
+			'state-changed',
+			AutohideVolume._autoupdate_visibility
+		);
+		this.h_oc2 = output._control.connect(
+			'active-output-update',
+			AutohideVolume._autoupdate_visibility
+		);
+		AutohideVolume._autoupdate_visibility();
 	}
-	menu._output.disconnect(h1);
-	_set_visibility(true);
-}
 
-function _set_visibility(show) {
-	let volume;
-	if (Main.panel.statusArea.quickSettings) {
-		volume = Main.panel.statusArea.quickSettings._volume;
-	} else {
-		volume = Main.panel.statusArea.aggregateMenu._volume;
+	disable() {
+		let output = panel.statusArea.quickSettings._volumeOutput;
+		let input = panel.statusArea.quickSettings._volumeInput;
+		if (this.h_ii1) input._input.disconnect(this.h_ii1);
+		if (this.h_ic1) input._control.disconnect(this.h_ic1);
+		if (this.h_ic2) input._control.disconnect(this.h_ic2);
+		if (this.h_ic3) input._control.disconnect(this.h_ic3);
+		if (this.h_ic4) input._control.disconnect(this.h_ic4);
+		if (this.h_oo1) output._output.disconnect(this.h_ii1);
+		if (this.h_oc1) output._control.disconnect(this.h_ic1);
+		if (this.h_oc2) output._control.disconnect(this.h_ic2);
+		AutohideVolume._set_visibility(true);
 	}
-	if (show) {
-		volume.show();
-	} else {
-		volume.hide();
+
+	static _set_visibility(show) {
+		let output = panel.statusArea.quickSettings._volumeOutput;
+		if (show) {
+			output.show();
+		} else {
+			output.hide();
+		}
 	}
-}
 
-function _autoupdate_visibility() {
-	let volume;
-	let menu;
-	if (Main.panel.statusArea.quickSettings) {
-		volume = Main.panel.statusArea.quickSettings._volume;
-		menu = volume;
-	} else {
-		volume = Main.panel.statusArea.aggregateMenu._volume;
-		menu = volume._volumeMenu;
-	}
-	let input_visible = volume._inputIndicator.visible;
+	static _autoupdate_visibility() {
+		let output = panel.statusArea.quickSettings._volumeOutput;
+		let input = panel.statusArea.quickSettings._volumeInput;
+		let input_visible = input.visible;
 
-	let output_muted = false;
-	try {
-		output_muted = menu._output._stream.is_muted;
-	} catch (e) {}  // fall back to displaying the icon
+		let output_muted = false;
+		try {
+			output_muted = output._output._stream.is_muted;
+		} catch (e) {}  // fall back to displaying the icon
 
-	try {
-		_set_visibility(!output_muted || input_visible);
-	} catch (e) {
-		logError(e, 'ExtensionErrorType');
+		try {
+			AutohideVolume._set_visibility(
+				!output_muted || input_visible
+			);
+		} catch (e) {
+			logError(e, 'ExtensionErrorType');
+		}
 	}
 }
